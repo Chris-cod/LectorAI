199c164db4b13379cd3f97d55893dc899ff0a0ad
vlemalieu <vlemalieu@stud.hs-bremen.de>
2024-06-08 18:18:08 +0200

    Icons zu schuelerdetails hinzufügen, schuelerbuttons klickbar machen

--- lib/seiten/Klasse/schuler_details.dart
+++ lib/seiten/Klasse/schuler_details.dart
@@ -18,8 +18,7 @@ class SchuelerDetails extends StatefulWidget {
   ShowSchuelerDetails createState() => ShowSchuelerDetails();
 }
 
-
-class ShowSchuelerDetails extends State<SchuelerDetails>{
+class ShowSchuelerDetails extends State<SchuelerDetails> {
   SchuelerInfo? schuelerInfo;
   Repository repository = Repository();
 
@@ -42,100 +41,107 @@ class ShowSchuelerDetails extends State<SchuelerDetails>{
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title:  Text('${schuelerInfo?.vorname?? 'max'} ${schuelerInfo?.nachname?? 'muster'}', style: const TextStyle(color: Colors.black), textAlign: TextAlign.center,),
+        title: Text(
+          '${schuelerInfo?.vorname ?? 'max'} ${schuelerInfo?.nachname ?? 'muster'}',
+          style: const TextStyle(color: Colors.black),
+          textAlign: TextAlign.center,
+        ),
         backgroundColor: const Color(0xff48CAE4),
       ),
-      body: schuelerInfo == null ? const Center(child: CircularProgressIndicator()) : Container(
-        padding: const EdgeInsets.all(10.0),
-        color: const Color(0xFF0077B6),
-        child: Column(
-          children: [
-          Container(
-            padding: const EdgeInsets.all(10.0),
-            width: 400,
-            height: 200,
-            decoration: BoxDecoration(
-              color: const Color(0xffCAF0F8),
-              borderRadius: BorderRadius.circular(25),
+      body: schuelerInfo == null
+          ? const Center(child: CircularProgressIndicator())
+          : Container(
+              padding: const EdgeInsets.all(10.0),
+              color: const Color(0xFFB9B5C6),
+              child: Column(
+                children: [
+                  Expanded(
+                    child: Container(
+                      padding: const EdgeInsets.all(10.0),
+                      decoration: BoxDecoration(
+                        color: const Color(0xffCAF0F8),
+                        borderRadius: BorderRadius.circular(25),
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: [
+                          buildInfoCartName('Erziehungsberechtigter', Icons.person),
+                          const SizedBox(height: 5.0),
+                          buildInfoField('Vorname', '${schuelerInfo!.kontakt.vorname}'),
+                          const SizedBox(height: 5.0),
+                          buildInfoField('Nachname', '${schuelerInfo!.kontakt.nachname}'),
+                          const SizedBox(height: 5.0),
+                          buildInfoField('E-mail', '${schuelerInfo!.kontakt.email}'),
+                          const SizedBox(height: 5.0),
+                          buildInfoField('Telefon', '${schuelerInfo!.kontakt.telefonnummer}'),
+                        ],
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 20.0),
+                  Expanded(
+                    child: Container(
+                      padding: const EdgeInsets.all(10.0),
+                      decoration: BoxDecoration(
+                        color: const Color(0xffCAF0F8),
+                        borderRadius: BorderRadius.circular(25),
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: [
+                          buildInfoCartName('Adresse', Icons.home),
+                          const SizedBox(height: 5.0),
+                          buildInfoField('Straße', '${schuelerInfo!.adresse.strasse}'),
+                          const SizedBox(height: 5.0),
+                          buildInfoField('Hausnummer', '${schuelerInfo!.adresse.hausnummer}'),
+                          const SizedBox(height: 5.0),
+                          buildInfoField('PLZ', '${schuelerInfo!.adresse.postleitzahl}'),
+                          const SizedBox(height: 5.0),
+                          buildInfoField('Stadt', 'Bremen'),
+                        ],
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 20.0),
+                  Expanded(
+                    child: Container(
+                      padding: const EdgeInsets.all(10.0),
+                      decoration: BoxDecoration(
+                        color: const Color(0xffCAF0F8),
+                        borderRadius: BorderRadius.circular(25),
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: [
+                          buildInfoCartName('AGs', Icons.group),
+                          const SizedBox(height: 5.0),
+                          buildInfoField('AG Wahl 1', '${schuelerInfo!.ags[0]}'),
+                          const SizedBox(height: 5.0),
+                          buildInfoField('AG Wahl 2', '${schuelerInfo!.ags[1]}'),
+                          const SizedBox(height: 5.0),
+                          buildInfoField('AG Wahl 3', '${schuelerInfo!.ags[2]}'),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
-            child: Column(
-              children: [
-                buildInfoCartName('Erziehungsberechtigte/r'),
-                const SizedBox(height: 5.0),
-                buildInfoField('Vorname', '${schuelerInfo!.kontakt.vorname}'),
-                const SizedBox(height: 5.0),
-                buildInfoField('Nachname', '${schuelerInfo!.kontakt.nachname}'),
-                const SizedBox(height: 5.0),
-                buildInfoField('E-mail', '${schuelerInfo!.kontakt.email}'),
-                const SizedBox(height: 5.0),
-                buildInfoField('Telefon', '${schuelerInfo!.kontakt.telefonnummer}'),
-              ]
-            )
-          ),
-          const SizedBox(height: 20.0),
-          Container(
-            padding: const EdgeInsets.all(10.0),
-            width: 400,
-            height: 200,
-            decoration: BoxDecoration(
-              color: const Color(0xffCAF0F8),
-              borderRadius: BorderRadius.circular(25),
-            ),
-            child: Column(
-              children: [
-                buildInfoCartName('Adresses'),
-                const SizedBox(height: 5.0),
-                buildInfoField('Straße', '${schuelerInfo!.adresse.strasse}'),
-                const SizedBox(height: 5.0),
-                buildInfoField('Hausnummer', '${schuelerInfo!.adresse.hausnummer}'),
-                const SizedBox(height: 5.0),
-                buildInfoField('PLZ', '${schuelerInfo!.adresse.postleitzahl}'),
-                const SizedBox(height: 5.0),
-                buildInfoField('Stadt', 'Bremen'),
-              ]
-            )
-          ),
-          const SizedBox(height: 20.0),
-          Container(
-            padding: const EdgeInsets.all(10.0),
-            width: 400,
-            height: 200,
-            decoration: BoxDecoration(
-              color: const Color(0xffCAF0F8),
-              borderRadius: BorderRadius.circular(25),
-            ),
-            child: Column(
-              children: [
-                buildInfoCartName('AGs'),
-                const SizedBox(height: 5.0),
-                buildInfoField('AG Wahl 1', '${schuelerInfo!.ags[0]}'),
-                const SizedBox(height: 5.0),
-                buildInfoField('AG Wahl 2', '${schuelerInfo!.ags[1]}'),
-                const SizedBox(height: 5.0),
-                buildInfoField('AG Wahl 3', '${schuelerInfo!.ags[2]}'),
-              ]
-            )
-          ),
-        ],
-        )
-        
-      ),
     );
   }
 
-   Widget buildInfoCartName(String cardName) {
-    return  SizedBox(
-          width: 175,
-          height: 20,
-          child: Align(
-            alignment: Alignment.topLeft,
-            child: Text(
-              cardName,
-              style: const TextStyle(color: Colors.black, fontSize: 15),
-          ),
+  Widget buildInfoCartName(String cardName, IconData icon) {
+    return Row(
+      children: [
+        Icon(icon, color: Colors.black),
+        const SizedBox(width: 5),
+        Text(
+          cardName,
+          style: const TextStyle(color: Colors.black, fontSize: 15, fontWeight: FontWeight.bold),
         ),
-      );
-    
+      ],
+    );
+
   }
 
   Widget buildInfoField(String label, String value) {
@@ -143,18 +149,14 @@ class ShowSchuelerDetails extends State<SchuelerDetails>{
       decoration: BoxDecoration(
         color: const Color(0xffADE8F4),
         borderRadius: BorderRadius.circular(25),
-      ), 
-      child:SizedBox(
-          width: 250,
-          height: 35,
-          child: Align(
-              alignment: Alignment.center,
-              child: Text(
-                '$label: $value',
-                style: const TextStyle(color: Colors.black, fontSize: 15),
-              )),
-        )
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Text(
+          '$label: $value',
+          style: const TextStyle(color: Colors.black, fontSize: 15),
+        ),
+      ),
     );
   }
 }
-
