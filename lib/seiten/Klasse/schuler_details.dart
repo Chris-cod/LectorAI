a670cbd077f0d2bd362eb392b8594513749f10e6
matrix <muazz.bdear@uze.mobility.com>
2024-06-20 16:16:42 +0200

    edit name list, card

--- lib/seiten/Klasse/schuler_details.dart
+++ lib/seiten/Klasse/schuler_details.dart
@@ -1,5 +1,3 @@
-
-
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
@@ -8,7 +6,6 @@ import 'package:lectorai_frontend/models/schueler_info.dart';
 import 'package:lectorai_frontend/services/repository.dart';
 
 class SchuelerDetails extends StatefulWidget {
-  
   const SchuelerDetails({super.key, required this.token, required this.schuelerId, required this.demoModus});
 
   final String token;
@@ -32,19 +29,14 @@ class ShowSchuelerDetails extends State<SchuelerDetails> {
   initList() async {
     SchuelerInfo schulerInformation;
     if (widget.demoModus) {
-      schulerInformation = await repository.fetchStudentInfoFromLocalJson(
-          widget.token, widget.schuelerId);
+      schulerInformation = await repository.fetchStudentInfoFromLocalJson(widget.token, widget.schuelerId);
+    } else {
+      schulerInformation = await repository.getStudentInformation(widget.token, widget.schuelerId);
     }
-    else {
-      schulerInformation =
-      await repository.getStudentInformation(widget.token, widget.schuelerId);
-    }
-    //schuelerInfo = alleSchueler.firstWhere((element) => element.id == widget.schuelerId);
     setState(() {
       schuelerInfo = schulerInformation;
     });
   }
-
 
   @override
   Widget build(BuildContext context) {
@@ -53,19 +45,14 @@ class ShowSchuelerDetails extends State<SchuelerDetails> {
     return Scaffold(
       appBar: AppBar(
         title: Text(
-          '${schuelerInfo?.vorname ?? 'max'} ${schuelerInfo?.nachname ??
-              'muster'}',
+          '${schuelerInfo?.vorname ?? 'max'} ${schuelerInfo?.nachname ?? 'muster'}',
           textAlign: TextAlign.center,
         ),
         leading: GestureDetector(
           onTap: () => Navigator.pop(context),
-          child: Container(
-            padding: const EdgeInsets.all(15.0),
-            height: 10,
-            child: Icon(
-              Icons.arrow_back,
-              color: theme.brightness == Brightness.dark ? Colors.white : Colors.black,  // Dynamically set color based on theme
-            ),
+          child: Icon(
+            Icons.arrow_back,
+            color: theme.brightness == Brightness.dark ? Colors.white : Colors.black,
           ),
         ),
       ),
@@ -73,79 +60,38 @@ class ShowSchuelerDetails extends State<SchuelerDetails> {
           ? const Center(child: CircularProgressIndicator())
           : Container(
         padding: const EdgeInsets.all(10.0),
-        child: Column(
+        child: ListView(
           children: [
-            Expanded(
-              child: Container(
-                padding: const EdgeInsets.all(10.0),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(25),
-                ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    buildInfoCartName('Erziehungsberechtigter', Icons.person),
-                    const SizedBox(height: 5.0),
-                    buildInfoField(
-                        'Vorname', '${schuelerInfo!.kontakt.vorname}'),
-                    const SizedBox(height: 5.0),
-                    buildInfoField(
-                        'Nachname', '${schuelerInfo!.kontakt.nachname}'),
-                    const SizedBox(height: 5.0),
-                    buildInfoField('E-mail', '${schuelerInfo!.kontakt.email}'),
-                    const SizedBox(height: 5.0),
-                    buildInfoField(
-                        'Telefon', '${schuelerInfo!.kontakt.telefonnummer}'),
-                  ],
-                ),
-              ),
+            buildCard(
+              context,
+              'Erziehungsberechtigter',
+              [
+                buildInfoField('Vorname', '${schuelerInfo!.kontakt.vorname}'),
+                buildInfoField('Nachname', '${schuelerInfo!.kontakt.nachname}'),
+                buildInfoField('E-mail', '${schuelerInfo!.kontakt.email}'),
+                buildInfoField('Telefon', '${schuelerInfo!.kontakt.telefonnummer}'),
+              ],
             ),
             const SizedBox(height: 20.0),
-            Expanded(
-              child: Container(
-                padding: const EdgeInsets.all(10.0),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(25),
-                ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    buildInfoCartName('Adresse', Icons.home),
-                    const SizedBox(height: 5.0),
-                    buildInfoField(
-                        'Straße', '${schuelerInfo!.adresse.strasse}'),
-                    const SizedBox(height: 5.0),
-                    buildInfoField(
-                        'Hausnummer', '${schuelerInfo!.adresse.hausnummer}'),
-                    const SizedBox(height: 5.0),
-                    buildInfoField(
-                        'PLZ', '${schuelerInfo!.adresse.postleitzahl}'),
-                    const SizedBox(height: 5.0),
-                    buildInfoField('Stadt', 'Bremen'),
-                  ],
-                ),
-              ),
+            buildCard(
+              context,
+              'Adresse',
+              [
+                buildInfoField('Straße', '${schuelerInfo!.adresse.strasse}'),
+                buildInfoField('Hausnummer', '${schuelerInfo!.adresse.hausnummer}'),
+                buildInfoField('PLZ', '${schuelerInfo!.adresse.postleitzahl}'),
+                buildInfoField('Stadt', 'Bremen'),
+              ],
             ),
             const SizedBox(height: 20.0),
-            Expanded(
-              child: Container(
-                padding: const EdgeInsets.all(10.0),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(25),
-                ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    buildInfoCartName('AGs', Icons.group),
-                    const SizedBox(height: 5.0),
-                    buildInfoField('AG Wahl 1', '${schuelerInfo!.ags[0]}'),
-                    const SizedBox(height: 5.0),
-                    buildInfoField('AG Wahl 2', '${schuelerInfo!.ags[1]}'),
-                    const SizedBox(height: 5.0),
-                    buildInfoField('AG Wahl 3', '${schuelerInfo!.ags[2]}'),
-                  ],
-                ),
-              ),
+            buildCard(
+              context,
+              'AGs',
+              [
+                buildInfoField('AG Wahl 1', '${schuelerInfo!.ags[0]}'),
+                buildInfoField('AG Wahl 2', '${schuelerInfo!.ags[1]}'),
+                buildInfoField('AG Wahl 3', '${schuelerInfo!.ags[2]}'),
+              ],
             ),
           ],
         ),
@@ -153,31 +99,39 @@ class ShowSchuelerDetails extends State<SchuelerDetails> {
     );
   }
 
-  Widget buildInfoCartName(String cardName, IconData icon) {
-    return Row(
-      children: [
-        const SizedBox(width: 5),
-        Text(
-          cardName,
-          style: const TextStyle(
-               fontSize: 15, fontWeight: FontWeight.bold),
+  Widget buildCard(BuildContext context, String title, List<Widget> children) {
+    return Card(
+      elevation: 2.0,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+      child: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              title,
+              style: const TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 5.0),
+            ...children,
+          ],
         ),
-      ],
+      ),
     );
   }
 
   Widget buildInfoField(String label, String value) {
-    var theme = Theme.of(context); // Accessing the current theme
+    var theme = Theme.of(context);
     return Container(
+      margin: const EdgeInsets.symmetric(vertical: 5.0),
       decoration: BoxDecoration(
-        color: theme.cardColor, // Use a suitable color from the theme
-        borderRadius: BorderRadius.circular(25),
+        color: theme.cardColor,
+        borderRadius: BorderRadius.circular(10),
         boxShadow: [
           BoxShadow(
             color: theme.shadowColor,
-            // Ensures the shadow is visible in both light and dark mode
-            blurRadius: 3,
-            offset: Offset(1, 1),
+            blurRadius: 4,
+            offset: Offset(0, 2),
           ),
         ],
       ),
@@ -185,11 +139,9 @@ class ShowSchuelerDetails extends State<SchuelerDetails> {
         padding: const EdgeInsets.all(8.0),
         child: Text(
           '$label: $value',
-          style: TextStyle(color: theme.textTheme.bodyMedium!
-              .color), // Ensuring text color is also theme-based
+          style: TextStyle(color: theme.textTheme.bodyMedium!.color),
         ),
       ),
     );
   }
-
 }
