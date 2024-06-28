82885c5558a080d2d40c681c7ef5d6eb058fb951
Ayham019 <almalla.ayh@gmail.com>
2024-06-28 04:24:15 +0200

    Anpassung der Klasse main, indem die Klasse ThemeManager erstellt wurde.

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:lectorai_frontend/seiten/Settings/theme_manager.dart';
 import 'package:lectorai_frontend/seiten/home/home.dart';
 import 'package:provider/provider.dart';
 import 'package:lectorai_frontend/seiten/Settings/theme_provider.dart';
 
-Future<void> main() async {
-  await dotenv.load(fileName: "assets/.env");
+void main() async {
+  await ConfigLoader.loadEnv();
   runApp(const MyApp());
+}
+
+class ConfigLoader {
+  static Future<void> loadEnv() async {
+    await dotenv.load(fileName: "assets/.env");
+  }
 }
 
 class MyApp extends StatelessWidget {
@@ -15,38 +22,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
-      create: (_) => ThemeProvider(
-        ThemeData.light().copyWith(
-          scaffoldBackgroundColor: const Color(0xFFF8F9FA), // Hintergrundfarbe
-          appBarTheme: const AppBarTheme(
-            backgroundColor: Color.fromARGB(255, 233, 229, 240), // AppBar-Farbe
-          ),
-          elevatedButtonTheme: ElevatedButtonThemeData(
-            style: ElevatedButton.styleFrom(
-              backgroundColor: const Color.fromARGB(255, 233, 229, 240), // Button-Farbe
-            ),
-          ),
-          inputDecorationTheme: InputDecorationTheme(
-            filled: true,
-            fillColor: const Color.fromARGB(255, 233, 229, 240), // Eingabefeld-Farbe
-            border: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(25.0),
-              borderSide: BorderSide.none,
-            ),
-          ),
-          iconTheme: const IconThemeData(
-          color: Color.fromARGB(255, 116, 82, 163),
-        ),
-        
-        ),
-      ),
+      create: (_) => ThemeManager.buildTheme(),
       child: Consumer<ThemeProvider>(
         builder: (context, themeProvider, child) {
           return MaterialApp(
             title: 'LectorAI',
-            debugShowCheckedModeBanner: false, // Entfernt das Debug-Banner
-            theme: themeProvider
-                .themeData, // Verwende das aktuelle Thema aus dem ThemeProvider
+            debugShowCheckedModeBanner: false,
+            theme: themeProvider.themeData,
             home: const StartPage(),
           );
         },
